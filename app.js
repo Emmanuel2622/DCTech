b398bcd82c8053cd78b018c5884e54e439e41e74
emmaa.den <72219992+Emmanuel2622@users.noreply.github.com>
2024-01-23 15:57:37 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
+
+addEventListener('DOMContentLoaded', () => {
+  const btn = document.querySelector('.btn')
+  if (btn) {
+    btn.addEventListener('click', () =>{
+      const mep1 = document.querySelector('.mep1')
+      mep1.classList.toggle('show')
+    })
+  }
+
+  const s1 = document.querySelector('.s1')
+  if (s1) {
+    s1.addEventListener('click', () =>{
+      const mep1 = document.querySelector('.mep1')
+      mep1.classList.toggle('show')
+    })
+  }
+
+  const s2 = document.querySelector('.s2')
+  if (s2) {
+    s2.addEventListener('click', () =>{
+      const mep1 = document.querySelector('.mep1')
+      mep1.classList.toggle('show')
+    })
+  }
+
+  const s3 = document.querySelector('.s3')
+  if (s3) {
+    s3.addEventListener('click', () =>{
+      const mep1 = document.querySelector('.mep1')
+      mep1.classList.toggle('show')
+    })
+  }
+
+  const s4 = document.querySelector('.s4')
+  if (s4) {
+    s4.addEventListener('click', () =>{
+      const mep1 = document.querySelector('.mep1')
+      mep1.classList.toggle('show')
+    })
+  }
+
+})
+
 let currentSlideIndex = 0;
 showSlides();
 
